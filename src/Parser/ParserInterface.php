9c755753f10154fd3400549fe1cda2f3008c9a3f
Kenji Suzuki <kenji.uui@gmail.com>
2019-10-20 21:48:52 +0900

    feat: update interface

--- src/Parser/ParserInterface.php
+++ src/Parser/ParserInterface.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Kenjis\ToggleTimeEntryPusher\Parser;
 
+use Kenjis\ToggleTimeEntryPusher\Line\AbstractLine;
+
 interface ParserInterface
 {
+    public function parse(string $lineString) : AbstractLine;
 }
