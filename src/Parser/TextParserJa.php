7735715071c2622fcaf34356227675f2926bd897
Kenji Suzuki <kenji.uui@gmail.com>
2019-10-20 18:37:52 +0900

    feat: add TextParserJa

---
+++ src/Parser/TextParserJa.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\ToggleTimeEntryPusher\Parser;
+
+use Kenjis\ToggleTimeEntryPusher\Exception\RuntimeException;
+use Kenjis\ToggleTimeEntryPusher\TimeEntry;
+
+class TextParserJa
+{
+    /**
+     * @var array [Project Code => Toggl PID]
+     */
+    private $pidMap;
+
+    /**
+     * @var array [Project Code Suffix => Toggl Tag Name]
+     */
+    private $tagMap;
+
+    public function __construct(array $pidMap, array $tagMap = [])
+    {
+        $this->pidMap = $pidMap;
+        $this->tagMap = $tagMap;
+    }
+
+    /**
+     * @return TimeEntry[]
+     */
+    public function parse(string $text) : array
+    {
+        $lines = explode("\n", $text);
+
+        $date = null;
+        $array = [];
+
+        foreach ($lines as $line) {
+            // Get date
+            $pattern = '!\A([0-9]{4}/[0-9]{2}/[0-9]{2})!u';
+            if (preg_match($pattern, $line, $matches)) {
+                $date = $matches[1];
+
+                continue;
+            }
+
+            // Get an entry
+            $pattern = '!\A([0-9]{2}:[0-9]{2})-([0-9]{2}:[0-9]{2})\[[0-9]+\] ([A-Z]+) (.*)!u';
+            if (preg_match($pattern, $line, $matches)) {
+                $start = $matches[1];
+                $stop = $matches[2];
+                $code = $matches[3];
+                $desc = $matches[4];
+
+                if ($date === null) {
+                    throw new RuntimeException('Cannot get date');
+                }
+
+                $entry = $this->createTimeEntry(
+                    $date,
+                    $code,
+                    $start,
+                    $stop,
+                    $desc
+                );
+
+                $array[] = $entry;
+
+                continue;
+            }
+        }
+
+        return $array;
+    }
+
+    private function createTimeEntry(
+        string $date,
+        string $code,
+        string $start,
+        string $stop,
+        string $desc
+    ) : TimeEntry {
+        if (! isset($this->pidMap[$code])) {
+            throw new RuntimeException('Cannot get pid: ' . $code);
+        }
+        $pid = $this->pidMap[$code];
+
+        $entry = new TimeEntry(
+            $date . ' ' . $start,
+            $date . ' ' . $stop
+        );
+
+        $entry->setTogglPid($pid);
+        $entry->setDescription($desc);
+
+        // Add tag for OPS
+        if (substr($code, -3) === 'OPS') {
+            if (! isset($this->tagMap['OPS'])) {
+                throw new RuntimeException('Cannot get tag name: ' . $code);
+            }
+
+            $entry->addTag($this->tagMap['OPS']);
+        }
+
+        return $entry;
+    }
+}
---
+++ tests/Parser/TestParserJaTest.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\ToggleTimeEntryPusher\Parser;
+
+use PHPUnit\Framework\TestCase;
+
+class TestParserJaTest extends TestCase
+{
+    public function testCanParseAnEntry() : void
+    {
+        $pidMap = [
+            'FOO' => 11111,
+        ];
+        $parser = new TextParserJa($pidMap);
+
+        $text = '2019/10/18（金）
+■本日の報告
+09:30-10:00[30] FOO #1110 クラス設計
+';
+        $array = $parser->parse($text);
+
+        $this->assertCount(1, $array);
+
+        $expected = [
+            'pid' => 11111,
+            'tags' => null,
+            'description' => '#1110 クラス設計',
+            'start' => '2019-10-18T09:30:00+09:00',
+            'stop' => '2019-10-18T10:00:00+09:00',
+            'duration' => 1800,
+        ];
+        $this->assertSame($expected, $array[0]->asArray());
+    }
+
+    public function testCanParseTwoEntries() : void
+    {
+        $pidMap = [
+            'FOO' => 11111,
+            'BAR' => 22222,
+        ];
+        $parser = new TextParserJa($pidMap);
+
+        $text = '2019/10/18（金）
+■本日の報告
+09:30-10:00[30] FOO #1110 クラス設計
+10:00-10:30[30] BAR #1111 レビュー
+';
+        $array = $parser->parse($text);
+        $test = [];
+        foreach ($array as $entry) {
+            $test[] = $entry->asArray();
+        }
+
+        $expected = [
+            [
+                'pid' => 11111,
+                'tags' => null,
+                'description' => '#1110 クラス設計',
+                'start' => '2019-10-18T09:30:00+09:00',
+                'stop' => '2019-10-18T10:00:00+09:00',
+                'duration' => 1800,
+            ],
+            [
+                'pid' => 22222,
+                'tags' => null,
+                'description' => '#1111 レビュー',
+                'start' => '2019-10-18T10:00:00+09:00',
+                'stop' => '2019-10-18T10:30:00+09:00',
+                'duration' => 1800,
+            ],
+        ];
+        $this->assertSame($expected, $test);
+    }
+
+    public function testCanParseAnEntryOfOperation() : void
+    {
+        $pidMap = [
+            'FOO' => 11111,
+            'FOOOPS' => 11111,
+        ];
+        $tagMap = [
+            'OPS' => '保守',
+        ];
+        $parser = new TextParserJa($pidMap, $tagMap);
+
+        $text = '2019/10/18（金）
+■本日の報告
+09:00-09:30[30] FOO #1111 クラス設計
+09:30-10:00[30] FOOOPS ログ確認
+';
+        $array = $parser->parse($text);
+        $test = [];
+        foreach ($array as $entry) {
+            $test[] = $entry->asArray();
+        }
+
+        $expected = [
+            [
+                'pid' => 11111,
+                'tags' => null,
+                'description' => '#1111 クラス設計',
+                'start' => '2019-10-18T09:00:00+09:00',
+                'stop' => '2019-10-18T09:30:00+09:00',
+                'duration' => 1800,
+            ],
+            [
+                'pid' => 11111,
+                'tags' => ['保守'],
+                'description' => 'ログ確認',
+                'start' => '2019-10-18T09:30:00+09:00',
+                'stop' => '2019-10-18T10:00:00+09:00',
+                'duration' => 1800,
+            ],
+        ];
+        $this->assertSame($expected, $test);
+    }
+}
