9d1dd924e70b5a0ddddf3e1bc073d688d822f9f4
Kenji Suzuki <kenji.uui@gmail.com>
2019-10-20 16:24:45 +0900

    feat: Toggl ProjectFetcher

---
+++ src/Toggl/ProjectFetcher.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\ToggleTimeEntryPusher\Toggl;
+
+use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
+
+class ProjectFetcher
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * Workspace ID
+     *
+     * @var int
+     */
+    private $wid;
+
+    /**
+     * @var string
+     */
+    private $api_token;
+
+    public function __construct(Client $client, int $wid, string $api_token)
+    {
+        $this->client = $client;
+        $this->wid = $wid;
+        $this->api_token = $api_token;
+    }
+
+    public function fetch() : ResponseInterface
+    {
+        // https://github.com/toggl/toggl_api_docs/blob/master/chapters/workspaces.md#get-workspace-projects
+        $uri = 'workspaces/' . $this->wid . '/projects';
+        $options = [
+            'auth' => [$this->api_token, 'api_token'],
+        ];
+
+        return $this->client->request('GET', $uri, $options);
+    }
+}
---
+++ tests/Toggl/ProjectFetcherTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\ToggleTimeEntryPusher\Toggl;
+
+require __DIR__ . '/../../conf/Config.php';
+
+use Config;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+
+class ProjectFetcherTest extends TestCase
+{
+    public function testCanFetchProject() : void
+    {
+        $client = $this->getMockBuilder(Client::class)
+            ->disableOriginalConstructor()
+            ->disableOriginalClone()
+            ->disableArgumentCloning()
+            ->disallowMockingUnknownTypes()
+            ->getMock();
+        $client->method('request')
+            ->willReturn(new Response());
+
+        $fetcher = new ProjectFetcher(
+            $client,
+            Config::WID,
+            Config::API_KEY
+        );
+
+        $response = $fetcher->fetch();
+        $this->assertInstanceOf(Response::class, $response);
+    }
+}
