9af5e58c130c4277fbef35fd48cc740ce8d0133b
Kenji Suzuki <kenji.uui@gmail.com>
2019-10-22 13:29:42 +0900

    style: composer cs-fix

--- src/LineOutputter.php
+++ src/LineOutputter.php
@@ -6,7 +6,7 @@ namespace Kenjis\ToggleTimeEntryPusher;
 
 class LineOutputter
 {
-    public function output(string $line)
+    public function output(string $line) : void
     {
         echo $line . PHP_EOL;
     }
