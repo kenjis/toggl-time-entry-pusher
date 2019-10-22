26555433d0100492240a2d0b440fadd1e30276bd
Kenji Suzuki <kenji.uui@gmail.com>
2019-10-22 21:27:49 +0900

    refactor: inject TextProccessor into PushTimeEntries

--- push-time-entries.php
+++ push-time-entries.php
@@ -7,9 +7,13 @@ require __DIR__ . '/conf/Config.php';
 
 use Kenjis\ToggleTimeEntryPusher\Command\PushTimeEntries;
 use Kenjis\ToggleTimeEntryPusher\Parser\TextParserJa;
+use Kenjis\ToggleTimeEntryPusher\TextProcessorFactory;
 
 $file = $argv[1];
 
 $parser = new TextParserJa();
-$command = new PushTimeEntries($parser);
+$factory = new TextProcessorFactory($parser);
+$processor = $factory->create();
+
+$command = new PushTimeEntries($processor);
 $command->run($file);
--- src/Command/PushTimeEntries.php
+++ src/Command/PushTimeEntries.php
@@ -4,25 +4,19 @@ declare(strict_types=1);
 
 namespace Kenjis\ToggleTimeEntryPusher\Command;
 
-use Config;
-use GuzzleHttp\Client;
 use Kenjis\ToggleTimeEntryPusher\Exception\RuntimeException;
-use Kenjis\ToggleTimeEntryPusher\LineOutputter;
-use Kenjis\ToggleTimeEntryPusher\Parser\ParserInterface;
 use Kenjis\ToggleTimeEntryPusher\TextProcessor;
-use Kenjis\ToggleTimeEntryPusher\TimeEntry\TimeEntryFactory;
-use Kenjis\ToggleTimeEntryPusher\Toggl\TimeEntryPusher;
 
 class PushTimeEntries
 {
     /**
-     * @var ParserInterface
+     * @var TextProcessor
      */
-    private $parser;
+    private $processor;
 
-    public function __construct(ParserInterface $parser)
+    public function __construct(TextProcessor $processor)
     {
-        $this->parser = $parser;
+        $this->processor = $processor;
     }
 
     public function run(string $file) : void
@@ -31,28 +25,7 @@ class PushTimeEntries
             throw new RuntimeException('Cannot read file: ' . $file);
         }
 
-        $client = new Client([
-            // Base URI is used with relative requests
-            'base_uri' => Config::TOGGL_BASE_URL,
-        ]);
-        $outputter = new LineOutputter();
-        $pusher = new TimeEntryPusher(
-            $client,
-            Config::API_KEY,
-            $outputter
-        );
-        $factory = new TimeEntryFactory(
-            Config::PID_MAP,
-            Config::TAG_MAP
-        );
-        $processor = new TextProcessor(
-            $this->parser,
-            $factory,
-            $pusher,
-            $outputter
-        );
-
-        $updatedText = $processor->process(file_get_contents($file));
+        $updatedText = $this->processor->process(file_get_contents($file));
         $updatedText = trim($updatedText) . "\n";
         file_put_contents($file, $updatedText, LOCK_EX);
     }
